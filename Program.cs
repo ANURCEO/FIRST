bbc28f3c2c16110975cbd16abae7c37571099c33
Semral05 <semralorucov05@gmail.com>
2024-02-14 21:13:14 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double number2 = 123.789;
+int result = (int)number2; // 123
+Console.WriteLine(result);
+
+int result2 = (int)((number2 - result) * 1000); // 789
+Console.WriteLine(result2);
+
+
+
+
+
